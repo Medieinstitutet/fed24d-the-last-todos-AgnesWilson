7d1f34521016ec9b71f3c98c366abf841b484741
AgnesWilson <agnes.wilson97@hotmail.com>
2025-06-02 15:12:32 +0200

    ✨ Added hard-coded tasks into the Todo file
    in case it is nessasary to have both hard-coded and input in the todo-list

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -4,8 +4,14 @@ import type { Todo } from "../models/todoType";
 import { InputNewTask } from "./InputNewTask";
 import { PrintTodo } from "./PrintTodos";
 
+const initialTodos: Todo[] = [
+  { id: 1, task: "Köp mjölk", isCompleted: false },
+  { id: 2, task: "Skriv klart rapporten", isCompleted: true },
+  { id: 3, task: "Städa lägenheten", isCompleted: false },
+];
+
 export const Todos = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
 
   const addTodo = (newTodo: Todo) => {
     setTodos([...todos, { ...newTodo, id: Date.now()}]);
