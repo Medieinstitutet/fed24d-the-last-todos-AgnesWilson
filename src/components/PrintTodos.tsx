48bab86d18151cd110e3f868b6d4af2306d9ab73
AgnesWilson <agnes.wilson97@hotmail.com>
2025-06-03 14:15:43 +0200

    💄 Added some styling

--- src/components/PrintTodos.tsx
+++ src/components/PrintTodos.tsx
@@ -9,13 +9,18 @@ type todoProps = {
 export const PrintTodo = ({ printedTodo, toggleCompleted, removeTodo }: todoProps) => {
 
     return <>
-    <div className="bg-[url('/assets/notes.png')] bg-cover min-h-[800px]">
-        <ul className="py-20">
+    <div className="bg-[url('/assets/notes.png')] bg-cover min-h-[800px] max-w-[400px]">
+        <ul className="py-24 px-10">
             {printedTodo.map((todo) => (
-                <li key={todo.id} className="list-style-none py-1.5">
-                    <h3 className="text-black">{todo.task}</h3>
+                <li key={todo.id} className="list-none py-1.5 grid grid-cols-[1fr_40px_40px] items-center gap-2">
+                    <h3 className={
+                            todo.isCompleted
+                            ? "[font-family:'Caveat',cursive] font-bold text-3xl text-gray-700 line-through"
+                            : "[font-family:'Caveat',cursive] font-bold text-3xl text-gray-700"
+                        }>{todo.task}
+                    </h3>
                     <input type="checkbox" checked={todo.isCompleted} onChange={() => toggleCompleted(todo.id)}/>
-                    <button onClick={() => removeTodo(todo.id)}>Ta bort</button>
+                    <button onClick={() => removeTodo(todo.id)}><span className="material-symbols-outlined text-black">delete</span></button>
                 </li>
             ))}
         </ul>
