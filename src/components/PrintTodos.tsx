f8baee3f59a3ae0c8455a656f18151c94cdb3f1b
AgnesWilson <agnes.wilson97@hotmail.com>
2025-06-03 12:29:51 +0200

    ✨ Added notes bg-img
    And started some basic styling

--- src/components/PrintTodos.tsx
+++ src/components/PrintTodos.tsx
@@ -9,16 +9,16 @@ type todoProps = {
 export const PrintTodo = ({ printedTodo, toggleCompleted, removeTodo }: todoProps) => {
 
     return <>
-
-    <ul>
-        {printedTodo.map((todo) => (
-            <li key={todo.id}>
-                <h3>{todo.task}</h3>
-                <input type="checkbox" checked={todo.isCompleted} onChange={() => toggleCompleted(todo.id)}/>
-                <button onClick={() => removeTodo(todo.id)}>Ta bort</button>
-            </li>
-        ))}
-    </ul>
-    <div className="bg"></div>
+    <div className="bg-[url('/assets/notes.png')] bg-cover min-h-[800px]">
+        <ul className="py-20">
+            {printedTodo.map((todo) => (
+                <li key={todo.id} className="list-style-none py-1.5">
+                    <h3 className="text-black">{todo.task}</h3>
+                    <input type="checkbox" checked={todo.isCompleted} onChange={() => toggleCompleted(todo.id)}/>
+                    <button onClick={() => removeTodo(todo.id)}>Ta bort</button>
+                </li>
+            ))}
+        </ul>
+    </div>
     </>
 }
