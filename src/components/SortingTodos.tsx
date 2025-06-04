fa893677d0cb8d833e2127e332e900295de71f07
AgnesWilson <agnes.wilson97@hotmail.com>
2025-06-04 14:31:19 +0200

    ✨ Added a-z/z-a sorting to the list
    Added sorting to the list that sorts the list based on the first letter of the task. The button toggles between A-Z (sv) and Z-A (sv)
    
    (i dont know if i can write svedish O here)

--- src/components/SortingTodos.tsx
+++ src/components/SortingTodos.tsx
@@ -2,15 +2,18 @@ import { Button } from "./Button"
 
 type Props = {
   setFilter: (filter: "All" | "Completed" | "NotCompleted") => void;
+  toggleSortOrder: () => void;
 };
 
-export const SortingTodos = ({setFilter}: Props) => {
+export const SortingTodos = ({setFilter, toggleSortOrder}: Props) => {
     return <>
         <div className="flex justify-center space-x-5">
 
             <Button onClick={() => setFilter("All")}>Alla</Button>
             <Button onClick={() => setFilter("Completed")}>Färdiga</Button>
             <Button onClick={() => setFilter("NotCompleted")}>Ofärdiga</Button>
+            
+            <button onClick={toggleSortOrder}><span aria-label="sortera A-Ö/Ö-A" className="material-symbols-outlined text-black">sort</span></button>
         </div>
     </>
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -8,7 +8,9 @@ import { initialTodos } from "../models/initialTodos";
 
 export const Todos = () => {
   const [todos, setTodos] = useState<Todo[]>(JSON.parse(localStorage.getItem("todos") || JSON.stringify(initialTodos)));
+
   const [filter, setFilter] = useState<"All" | "Completed" | "NotCompleted">("All");
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
 
   // Lägg till ny todo
   const addTodo = (newTodo: Todo) => {
@@ -32,17 +34,35 @@ export const Todos = () => {
   }
 
   // Filtrera vilka Todos som visas (alla, klara, oklara)
-  const filteredTodos = todos.filter(todo => {
+  // + sortera de i alfabetisk ordning (asc/ddesc) basetat på första bokstaven i "tasken"
+  const filteredTodos = todos
+  .filter(todo => {
     if (filter === "Completed") return todo.isCompleted;
     if (filter === "NotCompleted") return !todo.isCompleted;
     else return true;
   })
+  .sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.task.localeCompare(b.task, "sv");
+    } else {
+      return b.task.localeCompare(a.task, "sv");
+    }
+  })
+
+  // Togglar mellan A–Ö och Ö–A sortering
+  const toggleSortOrder = () => {
+    if (sortOrder === "asc") {
+      setSortOrder("desc");
+    } else {
+      setSortOrder("asc");
+    }
+  }
 
 
   localStorage.setItem("todos", JSON.stringify(todos));
 
   return <>
-  < SortingTodos setFilter={setFilter} />
+  < SortingTodos setFilter={setFilter} toggleSortOrder={toggleSortOrder}/>
   < InputNewTask addTodo={addTodo} />
   < PrintTodo printedTodo={filteredTodos} toggleCompleted={toggleCompleted} removeTodo={removeTodo}/>
   </>;
