ccace5b2900188540d025c4fa20272fcf1aee8ee
AgnesWilson <agnes.wilson97@hotmail.com>
2025-06-06 14:29:29 +0200

    💄 Final fixes with styling
    - moved the last css styling forcheckbox
    - small fixes with the placement of the bg and the bg-img

--- src/components/PrintTodos.tsx
+++ src/components/PrintTodos.tsx
@@ -9,7 +9,7 @@ type todoProps = {
 export const PrintTodo = ({ printedTodo, toggleCompleted, removeTodo }: todoProps) => {
 
     return <>
-    <div className="bg-[url('/assets/notes.png')] min-h-[800px] max-w-[400px] mx-auto">
+    <div className="bg-[url('/assets/notes.png')] bg-cover min-h-[700px] max-w-[400px] mx-auto">
         <ul className="py-24 px-10">
             {printedTodo.map((todo) => (
                 <li key={todo.id} className="list-none py-1.5 grid grid-cols-[1fr_1rem_1rem] items-center gap-x-10">
@@ -19,7 +19,10 @@ export const PrintTodo = ({ printedTodo, toggleCompleted, removeTodo }: todoProp
                             : "[font-family:'Caveat',cursive] font-bold text-3xl text-gray-700"
                         }>{todo.task}
                     </h3>
-                    <input type="checkbox" checked={todo.isCompleted} onChange={() => toggleCompleted(todo.id)}/>
+                    <input type="checkbox"
+                        className="cursor-pointer accent-green-600 w-16 h-5"
+                        checked={todo.isCompleted} onChange={() => toggleCompleted(todo.id)}
+                    />
                     <button onClick={() => removeTodo(todo.id)}>
                         <span aria-label="Ta bort todo" 
                             className="material-symbols-outlined text-black cursor-pointer 
